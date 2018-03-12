5c61405210f174ef3df7c04c761c76de274c25dc
KryniuPL <krzysztof.draganfm@gmail.com>
2018-03-12 21:10:10 +0100

    Generating Points works

--- src/controllers/GenerateDataController.java
+++ src/controllers/GenerateDataController.java
@@ -2,19 +2,14 @@ package controllers;
 
 
 
-import com.sun.org.apache.bcel.internal.classfile.ConstantUtf8;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
-
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class GenerateDataController {
 
@@ -22,24 +17,33 @@ public class GenerateDataController {
     public TextField NumberOfPoints;
 
     private List<Double> points;
+    private double rangeMin=0;
+    private double rangeMax=100;
 
     @FXML
     void generateData()
     {
         int IntegerPoints=Integer.parseInt(NumberOfPoints.getText());
+        double randomValueX=0;
+        double randomValueY=0;
+
         System.out.println(IntegerPoints);
         points=new ArrayList<>();
 
 
-
-        points.add(2.21);
-        points.add(22.32);
-
+        for (int i=0;i<IntegerPoints;i++) {
+            Random random=new Random();
+            randomValueX=rangeMin+(rangeMax-rangeMin)*random.nextDouble();
+            randomValueY=rangeMin+(rangeMax-rangeMin)*random.nextDouble();
+            points.add(randomValueX);
+            points.add(randomValueY);
+        }
         try {
             FileWriter writer=new FileWriter("points.txt");
             for (Double point: points)
             {
                 writer.write(point.toString());
+                writer.write(System.getProperty("line.separator"));
             }
             writer.close();
         } catch (IOException e) {
