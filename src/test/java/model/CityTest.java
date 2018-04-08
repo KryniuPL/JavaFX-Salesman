8a201dbf29937be6677a0d5c7b98d0695b947346
KryniuPL <krzysztof.draganfm@gmail.com>
2018-04-08 10:47:11 +0200

    Tests part 2

---
+++ src/test/java/model/CityTest.java
@@ -0,0 +1,26 @@
+package model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CityTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void start() {
+
+    }
+
+    @After
+    public void afterTest() {
+
+    }
+
+}
