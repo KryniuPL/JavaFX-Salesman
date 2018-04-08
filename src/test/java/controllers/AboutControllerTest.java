2a4f03391f1f2f589634523bf49f4d38e8eeeb10
KryniuPL <krzysztof.draganfm@gmail.com>
2018-04-08 10:45:34 +0200

    Lets try tests

---
+++ src/test/java/controllers/AboutControllerTest.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AboutControllerTest {
+
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void start() {
+
+    }
+
+    @After
+    public void afterTest() {
+
+    }
+}
